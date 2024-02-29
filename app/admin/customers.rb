eda3f8f4f069601d23745a46f9884897dba433b4
nfeng037 <nfeng@academic.rrc.ca>
2024-02-28 22:32:42 -0600

    Set up ActiveStorage to allow upload image

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,21 +1,53 @@
 ActiveAdmin.register Customer do
+  actions :index, :edit, :update, :create, :new, :destroy
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  actions :index, :edit, :update, :create, :destroy_admin_user_session_path
+  permit_params :full_name, :phone_number, :email, :notes, :image
 
-  permit_params :full_name, :phone_number, :email, :notes
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :phone_number
+    column :email
+    column "Image" do |customer|
+      if customer.image.attached?
+        image_tag customer.image.variant(resize_to_limit: [100, 100])
+      else
+        "No Image"
+      end
+    end
+    actions
+  end
 
+
+  filter :full_name
+  filter :phone_number
+  filter :email
+
+  form do |f|
+    f.inputs 'Customer Information' do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email
+      row :notes
+      row :image do |cust|
+        if cust.image.attached?
+          image_tag url_for(cust.image), width: "50%"
+        else
+          "No image attached"
+        end
+      end
+    end
+  end
 end
