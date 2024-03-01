4c2fce013ece5ea023ba1c2460da32fa715cb37f
nfeng037 <nfeng@academic.rrc.ca>
2024-03-01 14:06:43 -0600

    Adjust customer infomation display in ActiveAdmin Customer edit and show view

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -7,6 +7,7 @@ ActiveAdmin.register Customer do
     column :full_name
     column :phone_number
     column :email
+    column :notes
     column "Image" do |customer|
       if customer.image.attached?
         image_tag customer.image, width: "50px", height: "50px"
@@ -20,5 +21,32 @@ ActiveAdmin.register Customer do
   filter :full_name
   filter :phone_number
   filter :email
+  filter :notes
+
+
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email
+      f.input :notes
+      f.input :image, as: :file, hint: f.object.image.attached? ? "Current image: #{f.object.image.filename}" : ''
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email
+      row :notes
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag url_for(customer.image), width: "200px", height: "200px"
+        end
+      end
+    end
+  end
 
 end
