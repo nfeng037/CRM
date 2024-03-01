8379c2ffa443c38d301a3257577342af48775a1c
nfeng037 <nfeng@academic.rrc.ca>
2024-03-01 12:27:30 -0600

    Adjust image display size in ActiveAdmin index view

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -4,14 +4,12 @@ ActiveAdmin.register Customer do
   permit_params :full_name, :phone_number, :email, :notes, :image
 
   index do
-    selectable_column
-    id_column
     column :full_name
     column :phone_number
     column :email
     column "Image" do |customer|
       if customer.image.attached?
-        image_tag customer.image.variant(resize_to_limit: [100, 100])
+        image_tag customer.image, width: "50px", height: "50px"
       else
         "No Image"
       end
@@ -19,35 +17,8 @@ ActiveAdmin.register Customer do
     actions
   end
 
-
   filter :full_name
   filter :phone_number
   filter :email
 
-  form do |f|
-    f.inputs 'Customer Information' do
-      f.input :full_name
-      f.input :phone_number
-      f.input :email
-      f.input :notes
-      f.input :image, as: :file
-    end
-    f.actions
-  end
-
-  show do
-    attributes_table do
-      row :full_name
-      row :phone_number
-      row :email
-      row :notes
-      row :image do |cust|
-        if cust.image.attached?
-          image_tag url_for(cust.image), width: "50%"
-        else
-          "No image attached"
-        end
-      end
-    end
-  end
 end
